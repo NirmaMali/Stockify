8e4795e0f571561b58abe7cda99bff08722d0571
Nirma J Mali <nirmamali8618@gmail.com>
2026-04-09 02:37:54 +0530

    Created base template for each component + CDN for Bootstarp + Linked with fontawesom + added media

---
+++ frontend/src/landing_page/home/HomePage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Hero from './Hero';
+import Stats from './Stats';
+import Awards from './Awards';
+import Education from './Education';
+import Pricing from './Pricing';
+import OpenAccount from '../OpenAccount';
+import Footer from '../Footer';
+import Navbar from '../Navbar';
+
+
+function HomePage() {
+    return ( 
+        <>
+        <Navbar />
+        <Hero />
+        <Awards />
+        <Stats />
+        <Pricing />
+        <Education />
+        <OpenAccount />
+        <Footer />
+        </>
+     );
+}
+
+export default HomePage;
---
+++ frontend/src/landing_page/products/LeftSection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function LeftSection() {
+    return ( 
+        <h1>Left Section </h1>
+     );
+}
+
+export default LeftSection;

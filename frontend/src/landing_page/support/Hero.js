bfc84beba77d803c449937b8ad14aaf2b6b1c8b4
Nirma J Mali <nirmamali8618@gmail.com>
2026-04-09 22:23:07 +0530

    Support Section

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+  return (
+    <section id="supportHero">
+      <div className="supportWrapper">
+        <h5>Support Portal</h5>
+        <a href="#">Track Tickets</a>
+      </div>
+
+      <div className="supportContent">
+        {/* LEFT */}
+        <div className="left">
+          <h2>
+            Search for an answer or browse help topics to create a ticket
+          </h2>
+
+          <input
+            type="text"
+            placeholder="Eg: how do I activate F&O, why is my order getting rejected.."
+          />
+
+          <div className="links">
+            <a href="#">Track account opening</a>
+            <a href="#">Track segment activation</a>
+            <a href="#">Intraday margins</a>
+            <a href="#">Kite user manual</a>
+          </div>
+        </div>
+
+        {/* RIGHT */}
+        <div className="right">
+          <h3>Featured</h3>
+          <ol>
+            <li>
+              <a href="#">
+                Current Takeovers and Delisting - January 2024
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                Latest Intraday leverages - MIS & CO
+              </a>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;

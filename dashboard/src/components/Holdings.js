e23921681e12f36fbca2a8472d7b18977c8154ad
Nirma J Mali <nirmamali8618@gmail.com>
2026-04-13 21:07:56 +0530

    plotting graps in dasboard

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -32,10 +32,15 @@ const Holdings = () => {
   //   labels,
   //   datasets: [
   // {
-  //   label: 'Stock Name',
-  //   data: allHoldings.map((stock) => stock.price),
+  //   label: 'Dataset 1',
+  //   data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
   //   backgroundColor: 'rgba(255, 99, 132, 0.5)',
   // },
+  //     {
+  //       label: 'Dataset 2',
+  //       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+  //       backgroundColor: 'rgba(53, 162, 235, 0.5)',
+  //     },
   //   ],
   // };
 

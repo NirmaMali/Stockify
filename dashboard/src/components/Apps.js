bd24c77638f6a820a14a5f8597e245e84d86cf20
Nirma J Mali <nirmamali8618@gmail.com>
2026-05-09 22:43:35 +0530

    placeholder component

--- dashboard/src/components/Apps.js
+++ dashboard/src/components/Apps.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+
+// This is a placeholder component for the Apps page. You can replace it with your actual implementation.
 const Apps = () => {
   return <h1>Apps</h1>;
 };

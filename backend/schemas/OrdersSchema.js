0bccddea5a0893d78a6be62191e1613788c6688b
Nirma J Mali <nirmamali8618@gmail.com>
2026-05-09 23:24:01 +0530

    schema

--- backend/schemas/OrdersSchema.js
+++ backend/schemas/OrdersSchema.js
@@ -1,5 +1,7 @@
 const { Schema } = require("mongoose");
 
+
+//This schema defines the structure of the Orders collection in the MongoDB database. It includes fields for the name of the product, quantity, price, and mode of payment.
 const OrdersSchema = new Schema({
   name: String,
   qty: Number,
